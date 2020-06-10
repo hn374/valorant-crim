8d811018cd8b7d85bf181532a60675f23b738df2
Hoang Nguyen <mizore742@hotmail.com>
2020-06-10 15:01:15 -0400

    Changed styling

--- src/pages/SideSelect.js
+++ src/pages/SideSelect.js
@@ -5,13 +5,13 @@ function SideSelect() {
     return(
         <div className="sideSelectContainer">
             <h1 className="sideSelectHeader">Select Side</h1>
-            <div className="sideHeaderContainer">
-                <h2>Blue Defense</h2>
-                <h2>Red Attack</h2>
-            </div>
             <div className="sideCardContainer">
-                <a id="blueSide" className="sideCard"></a>
-                <a id="redSide" className="sideCard"></a>
+                <a id="blueSide" className="sideCard">
+                    <h2 id="attackHeader" className="sideHeader">Attack</h2>
+                </a>
+                <a id="redSide" className="sideCard">
+                    <h2 id="defenseHeader" className="sideHeader">Defense</h2>
+                </a>
             </div>
         </div>
     );
