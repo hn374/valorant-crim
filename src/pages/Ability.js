fe6d77483b0dc62d193676793674b3b896ac5ea2
Hoang Nguyen <mizore742@hotmail.com>
2020-06-11 21:00:59 -0400

    Added layout for ability page

--- src/pages/Ability.js
+++ src/pages/Ability.js
@@ -8,6 +8,38 @@ function Ability() {
                 <button className="abilityBackButton">Back</button>
                 <h1 className="abilityHeader">Abilities</h1>
             </div>
+            <HeroAbilities />
+            <TipsSection />
+            <TipsSection />
+            <TipsSection />
+            <TipsSection />
+        </div>
+    );
+}
+
+function HeroAbilities() {
+    return(
+        <div className="heroAbilitiesContainer">
+            <div className="heroAbility"></div>
+            <div className="heroAbility"></div>
+            <div className="heroAbility"></div>
+            <div className="heroAbility"></div>
+        </div>
+    );
+}
+
+function TipsSection() {
+    return(
+        <div className="tipsContainer">
+            <h1>Ability Name</h1>
+            <div className="tipsCardContainer">
+                <div className="tipCard"></div>
+                <div className="tipCard"></div>
+                <div className="tipCard"></div>
+                <div className="tipCard"></div>
+                <div className="tipCard"></div>
+                <div className="tipCard"></div>
+            </div>
         </div>
     );
 }
