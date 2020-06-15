f02950f379ee6b53cf4059ee0d0efbe9d9c3d0e7
Hoang Nguyen <mizore742@hotmail.com>
2020-06-14 21:45:48 -0400

    Added react scroll to ability pag

--- src/pages/Ability.js
+++ src/pages/Ability.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Ability.css';
+import * as Scroll from 'react-scroll';
+
+const ScrollLink = Scroll.Link;
+const ScrollElement = Scroll.Element;
 
 function Ability() {
     return(
         <div className="abilityContainer">
             <div className="abilityHeaderContainer">
-                {/* <button className="abilityBackButton">Back</button> */}
                 <Link to="/side-select" className="abilityBackButton">Back</Link>
                 <h1 className="abilityHeader">Abilities</h1>
             </div>
             <HeroAbilities />
-            <TipsSection />
-            <TipsSection />
-            <TipsSection />
-            <TipsSection />
+            <ScrollElement id="firstAbility" name="firstAbility">
+                <TipsSection />
+            </ScrollElement>
+            <ScrollElement id="secondAbility" name="secondAbility">
+                <TipsSection />
+            </ScrollElement>
+            <ScrollElement id="thirdAbility" name="thirdAbility">
+                <TipsSection />
+            </ScrollElement>
+            <ScrollElement id="fourthAbility" name="fourthAbility">
+                <TipsSection />
+            </ScrollElement>
         </div>
     );
 }
@@ -22,10 +33,38 @@ function Ability() {
 function HeroAbilities() {
     return(
         <div className="heroAbilitiesContainer">
-            <div className="heroAbility"></div>
-            <div className="heroAbility"></div>
-            <div className="heroAbility"></div>
-            <div className="heroAbility"></div>
+            <ScrollLink
+            to="firstAbility" 
+            spy={true} 
+            smooth={true} 
+            duration={500}
+            offset={-40}
+            className="heroAbility">
+            </ScrollLink>
+            <ScrollLink
+            to="secondAbility" 
+            spy={true} 
+            smooth={true} 
+            duration={500}
+            offset={-40}
+            className="heroAbility">
+            </ScrollLink>
+            <ScrollLink
+            to="thirdAbility" 
+            spy={true} 
+            smooth={true} 
+            duration={500}
+            offset={-40}
+            className="heroAbility">
+            </ScrollLink>
+            <ScrollLink
+            to="fourthAbility" 
+            spy={true} 
+            smooth={true} 
+            duration={500}
+            offset={-40}
+            className="heroAbility">
+            </ScrollLink>
         </div>
     );
 }
