7125c0ecdbdbd0310e50f4fa2cb270c379427a58
Hoang Nguyen <mizore742@hotmail.com>
2020-06-19 22:04:18 -0400

    Local storage

--- src/pages/HeroSelect.js
+++ src/pages/HeroSelect.js
@@ -5,6 +5,12 @@ import firebase from '../utils/fire';
 
 function HeroSelect() {
     const [heroes, setHeroes] = useState([]);
+    const [selectedHero, setSelectedHero] = useState('');
+
+    useEffect(() => {
+        localStorage.setItem('selectedHero', selectedHero);
+        console.log("THIS IS SELECTED", selectedHero);
+    }, [selectedHero]);
 
     useEffect(() => {
         // firebase.database().ref('/heroes').on('value', (snapshot) => {
@@ -30,23 +36,29 @@ function HeroSelect() {
         });
     }
 
+    function heroSelected(event) {
+        setSelectedHero(event.target.id);
+        console.log(event.target.id);
+        console.log(selectedHero);
+    }
+
     return(
         <div className="heroSelectContainer">
             <div className="heroHeaderContainer">
                 <h1 className="heroSelectHeader">Select Hero</h1>
             </div>
             <div className="heroCardContainer">
-                <Link to="/map-select" id="sage" className="heroCard"></Link>
-                <Link to="/map-select" id="breach" className="heroCard"></Link>
-                <Link to="/map-select" id="brimstone" className="heroCard"></Link>
-                <Link to="/map-select" id="cypher" className="heroCard"></Link>
-                <Link to="/map-select" id="jett" className="heroCard"></Link>
-                <Link to="/map-select" id="omen" className="heroCard"></Link>
-                <Link to="/map-select" id="phoenix" className="heroCard"></Link>
-                <Link to="/map-select" id="raze" className="heroCard"></Link>
-                <Link to="/map-select" id="reyna" className="heroCard"></Link>
-                <Link to="/map-select" id="sova"className="heroCard"></Link>
-                <Link to="/map-select" id="viper" className="heroCard"></Link>
+                <Link to="/map-select" id="sage" className="heroCard" onClick={heroSelected}></Link>
+                <Link to="/map-select" id="breach" className="heroCard" onClick={heroSelected}></Link>
+                <Link to="/map-select" id="brimstone" className="heroCard" onClick={heroSelected}></Link>
+                <Link to="/map-select" id="cypher" className="heroCard" onClick={heroSelected}></Link>
+                <Link to="/map-select" id="jett" className="heroCard" onClick={heroSelected}></Link>
+                <Link to="/map-select" id="omen" className="heroCard" onClick={heroSelected}></Link>
+                <Link to="/map-select" id="phoenix" className="heroCard" onClick={heroSelected}></Link>
+                <Link to="/map-select" id="raze" className="heroCard" onClick={heroSelected}></Link>
+                <Link to="/map-select" id="reyna" className="heroCard" onClick={heroSelected}></Link>
+                <Link to="/map-select" id="sova"className="heroCard" onClick={heroSelected}></Link>
+                <Link to="/map-select" id="viper" className="heroCard" onClick={heroSelected}></Link>
             </div>
         </div>
     );
