8b1a12c823c05c78fffa4762dafd34ddb695540a
Hoang Nguyen <mizore742@hotmail.com>
2020-06-25 15:41:00 -0400

    Saving changes

--- src/pages/AddTips.js
+++ src/pages/AddTips.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './AddTips.css';
 import { Link } from 'react-router-dom';
+import { Dropdown } from 'rsuite';
 
 function AddTips() {
     return(
@@ -8,6 +9,35 @@ function AddTips() {
             <div className="addTipsHeaderContainer">
                 <Link to="/add-post" className="addTipsBackButton">Back</Link>
                 <h1 className="addTipsHeader">Add Ability Tips</h1>
+            </div>
+            <div className="dropdownContainer">
+                <Dropdown className="heroDropdown" title="Select Hero">
+                    <Dropdown.Item>New File</Dropdown.Item>
+                    <Dropdown.Item>New File with Current Profile</Dropdown.Item>
+                    <Dropdown.Item>Download As...</Dropdown.Item>
+                    {/* Insert Heroes Here */}
+                </Dropdown>
+                <Dropdown className="mapDropdown" title="Select Map">
+                    <Dropdown.Item>New File</Dropdown.Item>
+                    <Dropdown.Item>New File with Current Profile</Dropdown.Item>
+                    <Dropdown.Item>Download As...</Dropdown.Item>
+                    {/* Insert Maps Here */}
+                </Dropdown>
+                <Dropdown className="sideDropdown" title="Select Side">
+                    <Dropdown.Item>Attack</Dropdown.Item>
+                    <Dropdown.Item>Defense</Dropdown.Item>
+                </Dropdown>
+                <Dropdown className="sideDropdown" title="Select Ability">
+                    <Dropdown.Item>Attack</Dropdown.Item>
+                    <Dropdown.Item>Defense</Dropdown.Item>
+                    {/* Insert Abilities Here */}
+                </Dropdown>
+                <div className="tipsInputContainer">
+                    <label className="tipsLabel">Select Tip Image</label>
+                    {/* <input className="tipsInput" type="text"></input> */}
+                    <input className="tipFileInput" type="file"></input>
+                </div>
+                <button className="addtipsButton">Add</button>
             </div>
         </div>
     );
